c2f7d135653bad6f4eeb8733cacafcb271eebed8
Futoren <kajimotoda1004@outlook.jp>
2022-07-14 22:59:33 +0900

    refacter api

--- mysite/api/models.py
+++ mysite/api/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
-class Todo(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.CharField(max_length=400)
+class Senser(models.Model):
+    x = models.FloatField(max_length=400)
+    y = models.FloatField(max_length=400)
+    z = models.FloatField(max_length=400)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Senser2(models.Model):
+    value = models.FloatField(max_length=400)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
