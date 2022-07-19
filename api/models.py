84cb3e2036bb4ac75f41d3807efc371732003776
Futoren <kajimotoda1004@outlook.jp>
2022-07-20 01:30:55 +0900

    change model

--- api/models.py
+++ api/models.py
@@ -1,16 +1,15 @@
 from django.db import models
 
+
 class Senser(models.Model):
+    Action = models.CharField(max_length=400)
+    SenserName = models.CharField(max_length=400)
     x = models.FloatField(max_length=400)
     y = models.FloatField(max_length=400)
     z = models.FloatField(max_length=400)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-class Senser2(models.Model):
-    value = models.FloatField(max_length=400)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
 def __str__(self):
-   return self.title
+    return self.title
